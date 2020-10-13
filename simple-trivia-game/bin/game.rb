fddb6e849fd99692c8d38ff2658e827d53eb5c74
Connie on Mac <21046498+CChirpy@users.noreply.github.com>
2020-10-13 01:23:00 -0400

    updated instructions

--- simple-trivia-game/bin/game.rb
+++ simple-trivia-game/bin/game.rb
@@ -8,10 +8,9 @@ set :public_folder, "static"
 set :views, "views"
 enable :sessions
 
-# trivia game should include at least 3 questions
-# all questions should appear on the same page
-# When a user answers the questions and submits the form,
-# a percentage of how many answers the user has answered correctly should be displayed.
+# include at least 3 questions
+# questions should appear on the same page
+# when user submits, show score in percentage
 
 get "/" do
   # When question is 1 score is 0
