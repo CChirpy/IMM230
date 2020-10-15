6d8431fb78c87fccbeb4c0cbc46bf30edb020047
Connie on Mac <21046498+CChirpy@users.noreply.github.com>
2020-10-15 02:10:28 -0400

    small adjustments to format

--- accessing-database/app.rb
+++ accessing-database/app.rb
@@ -1,15 +1,17 @@
-# Instructions: insert something into a SQLite database 1000 times
+require 'sqlite3'
+
+# Instructions: with ruby, insert something into a SQLite database 1000 times
 # Example: http://zetcode.com/db/sqliteruby/connect/
 # Using SQLite: sqlite3 test.db | .mode column | .headers on
 # sqlite> SELECT * FROM items;
 
-require 'sqlite3'
-
 begin
   # Connects to the test.db database.
   db = SQLite3::Database.open "test.db"
+
   # A new 'items' table is created if it does not already exist.
   db.execute "CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY)"
+
   # The execute method executes the given SQL statement.
   for i in 1..1000 do
     db.execute "INSERT INTO items VALUES(#{i})"
@@ -23,5 +25,4 @@ begin
   # Release the resources.
   ensure
     db.close if db
-
 end
