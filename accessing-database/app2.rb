34f4e97b6018fb7e1dfc2906c740e6a8b0b4ab10
Connie on Mac <21046498+CChirpy@users.noreply.github.com>
2020-10-20 06:38:53 -0400

    solved issue with .results_as_hash

--- accessing-database/app2.rb
+++ accessing-database/app2.rb
@@ -23,8 +23,9 @@ get '/' do
 end
 
 post '/insert' do
+  @db.results_as_hash = true
   user_input = params['user_input']
-  @db.execute "INSERT INTO items(input) VALUES('#{user_input}')"
+  @db.execute "INSERT INTO items (input) VALUES('#{user_input}')"
   @itemsupdate = @db.execute "SELECT * FROM items"
   erb :form
 end
