06f8c5d4cae0b03be85d82c51ea3650d7b58ff25
Connie on Mac <21046498+CChirpy@users.noreply.github.com>
2020-11-05 14:02:19 -0500

    added folders and assignment instructions

---
+++ FINAL-PROJECT/app.rb
@@ -0,0 +1,3 @@
+# Instructions: create a web application that persists data after the user closes the web browser,
+# app should allow the user to create, read, update, and delete data,
+# submit as zip file
